e2defcf8aa4ec73602aaf7e83018073019149b43
BibhuCool <90575664+BibhuCool@users.noreply.github.com>
2023-07-18 09:21:54 +0530

    StateDemoComponent.js

--- StateDemoComponent.js
+++ StateDemoComponent.js
@@ -4,6 +4,7 @@ export default function StatedemoComponent(){
 
     const[product,setProduct] = useState({name:'',price:0,city:'',stock:false})
     const[newProduct,setNewProduct] = useState({})
+    const[theme,setTheme] = useState({})
 
     
 
@@ -60,11 +61,31 @@ export default function StatedemoComponent(){
         })
     }
 
+    const changeTheme = (event) =>{
+        if(event.target.checked){
+            setTheme({
+            backgroundColor:'black',
+            color:'white'
+            })   
+        }
+        else{
+            setTheme({
+                backgroundColor:'lightcyan',
+                color:'black'
+            })
+        }
+       
+    }
+
     return(
         <>
-            <div className="container-fluid">
+            <div className="container-fluid mt-4">
             <div className="row">
             <div className="col-3">
+            <div style={theme} className="p-4 rounded">
+            <div className="form-switch">
+                <input type="checkbox" onChange={changeTheme} className="form-check-input" /> Dark Theme
+            </div>
             <h2>Register Product</h2>
             <dl>
                 <dt>Name</dt>
@@ -81,10 +102,12 @@ export default function StatedemoComponent(){
                 <dt>Stock</dt>
                 <dd className="form-switch"><input type="checkbox" onChange={HandleStockChange} className="form-check-input" /> Available</dd>
             </dl>
-            <button className="btn btn-primary w-100" onClick={SubmitClick}>Submit</button>
+            <button className="btn btn-primary w-100" onClick={SubmitClick}>Register</button>
+            </div>
 
             </div>
             <div className="col-9">
+            <div className="rounded" style={{backgroundColor:'black',color:'white',width:'330px',padding:'20px',height:'430px'}}>
             <h2>Product Details</h2>
             <dl>
                 <dt>Name</dt>
@@ -97,6 +120,7 @@ export default function StatedemoComponent(){
                 <dd>{(newProduct.stock===true)? "Available":"Out of stock"}</dd>
             </dl>
 
+            </div>
             </div>
 
             </div>
